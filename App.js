ff9d60ad31c89268b0d0701f459d0f268d1bb1ee
Murad Bin Makhashin <muradmm83@gmail.com>
2019-10-29 00:13:25 +0300

    Complete project

--- App.js
+++ App.js
@@ -1,19 +1,85 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { View, StatusBar } from 'react-native';
+import Deck from './components/Deck';
+import DeckList from './components/DeckList';
+import { createAppContainer } from 'react-navigation';
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+import reducers from './reducers';
+import middleware from './middleware';
+import Constants from 'expo-constants';
+import AddDeck from './components/AddDeck';
+import { receiveDecks } from './actions/decks';
+import { getDecks } from './utils/helpers';
+import DeckView from './components/DeckView';
+import AddCard from './components/AddCard';
+import Quiz from './components/Quiz';
+import Score from './components/Score';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const store = createStore(reducers, middleware);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const Tabs = createMaterialTopTabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'DECKS'
+    }
   },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'NEW DECK'
+    }
+  }
 });
+
+const Stack = createStackNavigator({
+  Home: {
+    screen: Tabs,
+    navigationOptions: {
+      header: null
+    }
+  },
+  Deck: {
+    screen: Deck
+  },
+  DeckView: {
+    screen: DeckView
+  },
+  AddCard: {
+    screen: AddCard
+  },
+  Quiz: {
+    screen: Quiz
+  },
+  Score: {
+    screen: Score,
+    navigationOptions: {
+      title: 'Score'
+    }
+  }
+});
+
+const Navigator = createAppContainer(Stack);
+
+export default class App extends Component {
+  componentDidMount() {
+    getDecks()
+      .then(decks => store.dispatch(receiveDecks(decks)));
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <View style={{ flex: 1 }}>
+          <View style={{ height: Constants.statusBarHeight }}>
+            <StatusBar barStyle="light-content" />
+          </View>
+          <Navigator />
+        </View>
+      </Provider>
+    );
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-### Mobile Flashcards
-This is my submission for *Mobilde Flashcards* project which is part of Udacity React Nanodegree course.
+# Mobile Flashcards
+
+This is my submition for Udacity's React Nanodegree **Mobile Flashcardds**.
+
+### Installation & Running
+
+Copy clone this repo
+
+```
+git clone https://github.com/muradmm83/mobile-flashcards-reactnd.git
+```
+CD to the repo local directory
+```
+cd mobile-flashcards-reactnd
+```
+Install required packages
+```
+npm i
+```
+Run the app
+```
+npm start
+```
+
+### Platform
+This RN app is using **Android** platform.
